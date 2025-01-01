5425e02f7fdde970e3787ad866a82d682941c86b
Evgeny <260443@mail.ru>
2025-01-01 23:39:29 +0500

    Booking section html + css layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -227,6 +227,32 @@
   </div>
 </div>
 
+<div class="booking-section section">
+  <div class="container">
+    <div class="content">
+      <div class="booking-section-title">Бронируйте туры<br>на сайте УралТур.ru</div>
+      <div class="easy-text">Это легко, всего 3 шага</div>
+      <ul class="list">
+        <li class="list-item">
+          <span class="step-number">Шаг 1: </span>
+          <span class="step-text"><a href="#" class="catalog-link">Выбираете тур</a>, даты и количество туристов</span>
+        </li>
+        <li class="list-item">
+          <span class="step-number">Шаг 2: </span>
+          <span class="step-text">Находим понравившийся тур</span>
+        </li>
+        <li class="list-item">
+          <span class="step-number">Шаг 3: </span>
+          <span class="step-text">Заполняем данные, ждём подтверждение и оплачиваем</span>
+        </li>
+      </ul>
+      <div class="booking-image">
+        <img src="/img/booking-image.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+
 <div class="container">
   <p>Товары</p>
   <div class="products">
