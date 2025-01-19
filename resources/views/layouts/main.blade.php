7edaf47d8c8ed769bfe049537f26619b26b88820
Evgeny <260443@mail.ru>
2025-01-19 22:08:25 +0500

    Fixed bottom menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -293,50 +293,41 @@
     </div>
   </div>
 
-  <!-- 
+
   <div class="fixed-bottom-menu">
     <div class="menu-wrapper">
       <div class="menu-item">
         <div class="menu-item__image">
-          <img src="/img/fixed-bottom-menu-house.svg" alt="">
+          <img src="/img/fixed-bottom-menu-bus.svg" alt="">
         </div>
-        <div class="menu-item__title">Главная</div>
-        <a href="/" class="full-link"></a>
+        <div class="menu-item__title">Туры</div>
+        <a href="/catalog" class="full-link"></a>
       </div>
       <div class="menu-item">
         <div class="menu-item__image">
-          <img src="/img/fixed-bottom-menu-rectangle.svg" alt="">
+          <img src="/img/fixed-bottom-menu-cards.svg" alt="">
         </div>
-        <div class="menu-item__title">Каталог</div>
-        <a href="/catalog" class="full-link"></a>
+        <div class="menu-item__title">Подборки</div>
+        <a href="#" class="full-link"></a>
       </div>
-      <div class="menu-item cart-menu-item">
+      <div class="menu-item">
         <div class="menu-item__image">
-          <img src="/img/fixed-bottom-menu-cart.svg" alt="">
+          <img src="/img/fixed-bottom-menu-confetti.svg" alt="">
         </div>
-        <div class="menu-item__title">Корзина</div>
-        <div id="mobile-cart-counter" class="badge-counter {{ isset($cart_count) ? 'active' : '' }}">{{ isset($cart_count) ? $cart_count : '' }}</div>
-        <a href="/cart" class="full-link"></a>
+        <div class="menu-item__title">Акции</div>
+        <a href="#" class="full-link"></a>
       </div>
-      <div class="menu-item cart-menu-item">
+      <div class="vertikal-line"></div>
+      <div class="menu-item">
         <div class="menu-item__image">
-          <img src="/img/fixed-bottom-menu-heart.svg" alt="">
+          <img src="/img/fixed-bottom-menu-fire.svg" alt="">
         </div>
-        <div class="menu-item__title">Избранное</div>
-        <div id="mobile-favourites-counter" class="badge-counter {{ isset($favourites_count) ? 'active' : '' }}">{{ isset($favourites_count) ? $favourites_count : '' }}</div>
-        <a href="/favourites" class="full-link"></a>
-      </div>
-      <div class="menu-item cart-menu-item">
-        <div class="menu-item__image">
-          <img src="/img/fixed-bottom-menu-chart.svg" alt="">
-        </div>
-        <div class="menu-item__title">Сравнение</div>
-        <div id="mobile-comparison-counter" class="badge-counter {{ isset($comparison_count) ? 'active' : '' }}">{{ isset($comparison_count) ? $comparison_count : '' }}</div>
-        <a href="/comparison" class="full-link"></a>
+        <div class="menu-item__title">Горящие туры</div>
+        <a href="#" class="full-link"></a>
       </div>
     </div>
   </div>
-   -->
+
 
   @if(Auth::check())
     @if (Auth::user()->isAdmin())
